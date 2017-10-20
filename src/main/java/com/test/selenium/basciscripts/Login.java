dbf6a49a07a0c8ad973c9d3e188635d471f2f374
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-10-20 12:03:25 +0530

    addind code

---
+++ src/main/java/com/test/selenium/basciscripts/Login.java
@@ -0,0 +1,31 @@
+package com.test.selenium.basciscripts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	WebDriver driver;
+	
+	@Test
+	public void testLogin(){
+		//java.lang.IllegalStateException: The path to the driver executable must be set by the webdriver.chrome.driver system property;
+		//java.lang.IllegalStateException: The driver is not executable: /Users/bsingh5/git/seleniumScripts/drivers/chromedriver
+		System.out.println(System.getProperty("user.dir"));
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver");
+		driver = new ChromeDriver();
+		//window
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+		
+		driver.get("http://172.16.125.9:8888/");
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@id='form']/div/div[2]/input")).sendKeys("admin");
+		driver.findElement(By.xpath("//*[@id='form']/div/div[4]/input")).sendKeys("password");
+		driver.findElement(By.id("submitButton")).click();
+	}
+
+}
