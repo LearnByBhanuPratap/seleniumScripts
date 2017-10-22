5ad1b8d625bf42f8715103cb1c3f4b42bebc443e
Bhanu Pratap <info.bhanupratap@gmail.com>
2017-10-22 00:31:11 -0700

    git test

---
+++ src/main/java/com/test/selenium/basciscripts/GitTest.java
@@ -0,0 +1,15 @@
+package com.test.selenium.basciscripts;
+
+public class GitTest {
+	
+	public void add(int add){
+		System.out.println(6);
+		int c =2+3;
+		System.out.println(c);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
