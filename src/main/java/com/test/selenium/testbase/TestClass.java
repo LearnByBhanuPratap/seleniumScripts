61103cc91a86ac89640f36b767bc4e2eaff013c4
Bhanu Pratap <info.bhanupratap@gmail.com>
2017-12-30 03:30:16 -0800

    added git commands

--- src/main/java/com/test/selenium/testbase/TestClass.java
+++ src/main/java/com/test/selenium/testbase/TestClass.java
@@ -2,6 +2,16 @@ package com.test.selenium.testbase;
 
 public class TestClass {
 	
+	/*
+	git add filename
+	git add .
+	
+	git commit -m"comments"
+	
+	git push
+	
+	*/
+	
 	public static void main(String[] args) {
 		
 	}
